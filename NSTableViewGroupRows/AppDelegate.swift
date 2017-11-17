f32376197209f39e9c91c5b0fce3a80f1a602736
Tobias Dunkel <tobiasdunkel@gmail.com>
2017-11-17 15:26:12 +0100

    Initial Commit

---
+++ NSTableViewGroupRows/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NSTableViewGroupRows
+//
+//  Created by Tobias Dunkel on 17.11.17.
+//  Copyright © 2017 Tobias Dunkel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
